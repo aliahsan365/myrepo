a64dff97e8caa71ec04468ebcfb884401d4bc0be
aliahsan365 <aliahsan365@gmail.com>
2025-06-19 03:27:02 +0200

    adding solution and permutations counters to placing_towers

--- 2025/June/placing_towers.cpp
+++ 2025/June/placing_towers.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 using namespace std;
 
+int counter = 0;
+int solutions = 0;
+
 //N Towers placed on a NxN chessboard without threading each other.
 
 //Key idea: To use both unidimensional array position  and content to represent tower position (i,j) on a chessboard.
@@ -40,12 +43,15 @@ bool check_constraint_all_different(const vector<int>& sol) {
 //Pure computation of ALL permutations, using backtracking, with no heuristics and no extra structures. 
 void permutation_with_backtraking(int depth, vector<int>& sol) {
     int N = sol.size();
-    if (depth == N - 1) {
-        if (check_constraint_all_different(sol))
-            print_pretty_board(sol);   
+    if (depth == N) {
+        ++counter;
+        if (check_constraint_all_different(sol)) {
+            print_pretty_board(sol);
+            ++solutions;    
+        }
     }
     for (int i = 0; i < N; ++i) {
-        if (depth < N ) {
+        if (depth < N) {
             sol[depth] = i;
             permutation_with_backtraking(depth + 1, sol);
         }
@@ -54,7 +60,9 @@ void permutation_with_backtraking(int depth, vector<int>& sol) {
 
 
 int main() {
-    int N = 9;
+    int N = 4;
     vector<int> sol(N, -1);
     permutation_with_backtraking(0, sol);
+    cout << "number posible solutions checked: " << counter << endl;
+    cout << "number of solutions founded: " << solutions << endl;  
 }
