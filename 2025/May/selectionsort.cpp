31266f01e11571f37bbb90f80ea1c68754b483f7
aliahsan365 <aliahsan365@gmail.com>
2025-06-20 11:32:43 +0200

    code refactor: creating functions, fixing comments msg

--- 2025/May/selectionsort.cpp
+++ 2025/May/selectionsort.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
 #include <vector>
 using namespace std;
 
-//read only (const), does not find the smallest element. Modifications are not allowed.
+
+//Modify the position of elements within the vector. Modifications are allowed (no const).
+void swap(vector<int>& v, int i, int j) {
+    int tmp = v[j];
+    v[j] = v[i];
+    v[i] = tmp;
+}
+
+//Does find the position of the smallest element from init position to N. 
+//Modifications are not allowed (Read Only).
+void swap(vector<int>& v, int i, int j) {
+    int tmp = v[j];
+    v[j] = v[i];
+    v[i] = tmp;
+}
+
 int find_smallest_pos(const vector<int>& v, int ini) {
     int pos = ini;
     int n = v.size();
@@ -12,21 +26,25 @@ int find_smallest_pos(const vector<int>& v, int ini) {
             pos = i;
     return pos;
 }
-//modify the position of elements within the vector. Modifications are allowed (no const).
-void swap(vector<int>& v, int i, int j) {
-    int tmp = v[j];
-    v[j] = v[i];
-    v[i] = tmp;
+
+void selection_sort(vector<int>& v) {
+    int n = v.size();
+    for (int i = 0; i < n; ++i) {
+        int pos = find_smallest_pos(v, i);
+        swap(v, i, pos);
+    }
 }
 
-int main() {
-    vector<int> v1 = { 11,2,34,45,6,6,7,3,34,5,6,-1,2,34,5,-34,0 };
-    int n = v1.size();
-    for (int i = 0; i < n; ++i) {
-        int pos = find_smallest_pos(v1, i);
-        swap(v1, i, pos);
-    }
+void print_vector(const vector<int>& v) {
+    int n = v.size();
     for (int i = 0; i < n; ++i)
-        cout << v1[i] << " ";
+        cout << v[i] << " ";
     cout << endl;
+}
+
+
+int main() {
+    vector<int> v = { 11,2,34,45,6,6,7,3,34,5,6,-1,2,34,5,-34,0 };
+    selection_sort(v);
+    print_vector(v);
 }
